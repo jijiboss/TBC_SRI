751309fa12d828fdbb57bfc9124b1fdf22a3c60d
jijiboss <maroki.sugimoto@outlook.com>
2017-10-24 19:58:41 -0400

    Created some hellow world pages About to create grid page

--- tbc_sri_app/urls.py
+++ tbc_sri_app/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+#
+from . import views
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'tbc_sri.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    #
-    url(r'^tbc_sri_app/', include('tbc_sri_app.urls')),
+    # ex: /polls/5/
+#    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'(?P<namae>)/$', views.helloWorld, name='helloWorld'),
+
 ]
--- tbc_sri_app/views.py
+++ tbc_sri_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+#
+from django.http import HttpResponse
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
