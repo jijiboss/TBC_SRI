d46b48ad392a5c8db766e307ae9fb72b18b3cb8a
jijiboss <maroki.sugimoto@outlook.com>
2017-11-17 21:06:15 -0500

    Up to tutorial 2 API Views. PUT working with the JSONParser

--- tbc_sri_app/views.py
+++ tbc_sri_app/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.template import loader
-from .models import lnos_statusPipeLine
-from django.forms import model_to_dict
-from django.http import HttpResponse, JsonResponse
+#from django.forms import model_to_dict
+#from django.http import HttpResponse #replaced by .Response
+#from django.http import JsonResponse #replaced by .Response
 from django.http import HttpRequest #https://docs.djangoproject.com/en/dev/ref/request-response/
 from django.core import serializers
 import json
 from django.views.decorators.csrf import ensure_csrf_cookie
+
+from .models import lnos_statusPipeLine
 from .serializers import lnosStatusPipeLineSerializer
 #=================================
 #for REST framework
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 #=================================
 #for debugging
 #https://pythonconquerstheuniverse.wordpress.com/2009/09/10/debugging-in-python/
@@ -39,65 +44,112 @@ def sritable3(request):
 #   python manage.py shell
 #Take 2 in attept to understand REST better
 #http://www.django-rest-framework.org/tutorial/1-serialization/#using-modelserializers
+@api_view(['GET', 'POST'])
 def myLoadData02(request):
     #List all existing or create new
+    print("===============================================")
     if request.method == 'GET':
         cargo = lnos_statusPipeLine.objects.all()
         serializer = lnosStatusPipeLineSerializer(cargo, many = True)
-        return JsonResponse(serializer.data, safe = False)
+        print("===============================================")
+        #return JsonResponse(serializer.data, safe = False) #Replaced by .Response and @api_view
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = lnosStatusPipeLineSerializer(data = data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status = 201)
-        return JsonResponse(serializer.errors, status = 400)
-
-def myUpdateData02(request, pk):
-
-    """
-        Because I need to pass pk, need to update the JS with the following...
-
-            var thisPK = item["pk"] //need to get the pk to pass with the PUT request
-            ...
-            url: 'myUpdateData02/' + thisPK,
-            ...
-
-        Then update urls.py accordingly
-
-            url(r'^myUpdateData02/(?P<pk>[0-9]+)$', views.myUpdateData02, name='myUpdateData02'),
-    """
-
-    #Retrieve, update or delete
-
-    print('***********')
-    #try getting the 'pk' instance
-    try:
-        cargo = lnos_statusPipeLine.objects.get(pk = pk)
-    #return Not FOund 404 if not found
-    except lnos_statusPipeLine.DoesNotExist:
-        return HttpResponse(status = 404)
-
-    if request.method == 'GET':
-        #Serializer will return the instance object in Python native format
-        serializer = lnosStatusPipeLineSerializer(cargo)
-        #Return JSON-fied representation of the instance data
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
         #JSONParser() => parse request content that is in JSON format
         #.parse(request) => parse stream-like object representing the body of the request
         #http://www.django-rest-framework.org/api-guide/parsers/#custom-parsers
         #parse the stream into Python native data type
-        data = JSONParser().parse(request)
         #then turn that into a dictionary of validated data
-        serializer = lnosStatusPipeLineSerializer(cargo, data = data)
+        print("LoadData:POST request => ", request)
+        data = JSONParser().parse(request) #dict at this point
+        print("LoadData:POST data => ", data)
+        serializer = lnosStatusPipeLineSerializer(data = data)
+        """
+        #Supposedly no longer necessary to manually convert input stream into dict after using the api_view
+        #instead simply call the serializer passing the request.data as below
+        serializer = lnosStatusPipeLineSerializer(data=request.data)
+        """
+        print("LoadData:POST attempt => ", serializer)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status = 400)
+            print("LoadData:POST successful => ", serializer)
+            print("===============================================")
+            #return JsonResponse(serializer.data, status = 201) #replaced by .Response and .status and @api_view
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        print("===============================================")
+        #return JsonResponse(serializer.errors, status = 400) #replaced by .Response and .status and @api_view
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def myUpdateData02(request, pk):
+
+    print("===============================================")
+    """
+        #Retrieve, update or delete
+        Because I need to pass pk, need to update the JS with the following...
+            var thisPK = item["pk"] //need to get the pk to pass with the PUT request
+            ...
+            url: 'myUpdateData02/' + thisPK,
+            ...
+        Then update urls.py accordingly
+            url(r'^myUpdateData02/(?P<pk>[0-9]+)$', views.myUpdateData02, name='myUpdateData02'),
+    """
+
+    #try getting the 'pk' instance
+    try:
+        cargo = lnos_statusPipeLine.objects.get(pk = pk)
+    #return Not Found 404 if not found
+    except lnos_statusPipeLine.DoesNotExist:
+        print("===============================================")
+        #return HttpResponse(status = 404) #replaced by .Response and .status and @api_view
+        return Response(status = status.HTTP_404_NOT_FOUND)
+
+    #GET
+    if request.method == 'GET':
+        #Serializer will return the instance object in Python native format
+        serializer = lnosStatusPipeLineSerializer(cargo)
+        #Return JSON-fied representation of the instance data
+        print("===============================================")
+        #return JsonResponse(serializer.data) #replaced by .Response and @api_view
+        return Response(serializer.data)
+
+    #PUT
+    elif request.method == 'PUT':
+
+        #JSONParser() => parse request content that is in JSON format
+        #.parse(request) => parse stream-like object representing the body of the request
+        #parse the stream into Python native data type
+        #then turn that into a dictionary of validated data
+        #LoadData:PUT attempt =>  {'pk': 25, 'mbol': 'mbol01', 'container': 'container02'}
+        #this updates fine
+        data = JSONParser().parse(request) #dict at this point
+        print("LoadData:PUT attempt => ", data)
+        serializer = lnosStatusPipeLineSerializer(cargo, data = data)
+
+        """
+        #Supposedly no longer necessary to manually convert input stream into dict after using the api_view
+        #But this does not update as expected.
+        #request.data returns a QueryDict instead of a dict
+        serializer = lnosStatusPipeLineSerializer(cargo, data = request.data)
+        print("LoadData:PUT attempt => ", request.data)
+        #LoadData:PUT attempt =>  <QueryDict: {'{"pk":25,"mbol":"mbol01","container":"container02"}': ['']}>
+        #Tried dict(request.data) which would pass this
+        #LoadData:PUT attempt =>  {'{"pk":25,"mbol":"mbol01","container":"container02"}': ['']}
+        """
+
+        if serializer.is_valid():
+            serializer.save()
+            print("UpdateData:PUT successful => ", serializer.data)
+            print("===============================================")
+            #return JsonResponse(serializer.data)  #replaced by .Response and @api_view
+            return Response(serializer.data)
+        print("===============================================")
+        #return JsonResponse(serializer.errors, status = 400)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         cargo.delete()
-        return HttpResponse(status = 204)
+        print("===============================================")
+        #return HttpResponse(status = 204) #replaced by .Response and .status and @api_view
+        return Response(status = status.HTTP_204_NO_CONTENT)
